f35d283e82159d1b218c6744aadc39a15dd07802
teamclouday <teamclouday@gmail.com>
2025-05-05 00:24:39 +0800

    add modelscope support

--- download.py
+++ download.py
@@ -4,6 +4,9 @@ from pathlib import Path
 
 from dotenv import find_dotenv, load_dotenv
 from huggingface_hub.hf_api import HfApi
+from modelscope import HubApi
+from modelscope.hub.file_download import model_file_download
+from modelscope.hub.snapshot_download import snapshot_download
 
 load_dotenv(find_dotenv(".env.local"))
 load_dotenv(find_dotenv(".env"))
@@ -39,10 +42,16 @@ parser.add_argument(
     default=None,
     help="Comma-separated list of file extensions to exclude from the download.",
 )
+parser.add_argument(
+    "--modelscope",
+    action="store_true",
+    help="Use this flag if the model is from ModelScope.",
+)
 
 args = parser.parse_args()
 
-api = HfApi(token=os.getenv("HF_TOKEN"))
+api_hf = HfApi(token=os.getenv("HF_TOKEN"))
+api_ms = HubApi()
 
 model_id = args.input
 output_dir = Path("models") / (
@@ -50,17 +59,36 @@ output_dir = Path("models") / (
 )
 output_dir.mkdir(parents=True, exist_ok=True)
 
+if args.modelscope:
+    api_ms.login(os.getenv("MODELSCOPE_TOKEN"))
+
 if args.file:
     print(f"Downloading {args.file} from {model_id} to {output_dir}")
-    api.hf_hub_download(repo_id=model_id, filename=args.file, local_dir=output_dir)
+    if args.modelscope:
+        model_file_download(
+            model_id=model_id,
+            file_path=args.file,
+            local_dir=output_dir,
+        )
+    else:
+        api_hf.hf_hub_download(
+            repo_id=model_id, filename=args.file, local_dir=output_dir
+        )
 
 else:
     print(f"Downloading {model_id} to {output_dir}")
-    api.snapshot_download(
-        repo_id=model_id,
-        local_dir=output_dir,
-        local_dir_use_symlinks=False,
-        ignore_patterns=args.exclude.split(",") if args.exclude else None,
-    )
+    if args.modelscope:
+        snapshot_download(
+            model_id=model_id,
+            local_dir=output_dir,
+            ignore_file_pattern=args.exclude.split(",") if args.exclude else None,
+        )
+    else:
+        api_hf.snapshot_download(
+            repo_id=model_id,
+            local_dir=output_dir,
+            local_dir_use_symlinks=False,
+            ignore_patterns=args.exclude.split(",") if args.exclude else None,
+        )
 
 print("Done!")
